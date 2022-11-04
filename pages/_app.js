5bf9200c46cc42709ab84906222e3010cac8fad2
Ekweaga <hikay133@gmail.com>
2022-11-04 12:02:39 +0100

    Firebase integration,authentication

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import '../styles/globals.css'
+import { AuthProvider } from './components/Context'
 import Layout from './components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (<Layout>
+    <AuthProvider>
     <Component {...pageProps} />
+    </AuthProvider>
     </Layout>)
 }
 
---
+++ pages/components/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCjWLiCK5CzgHS-dndakAspzrqFRTIGEuo",
+  authDomain: "melodax-93b59.firebaseapp.com",
+  projectId: "melodax-93b59",
+  storageBucket: "melodax-93b59.appspot.com",
+  messagingSenderId: "968954038181",
+  appId: "1:968954038181:web:6d2ee059712caafd068a4c",
+  measurementId: "G-11GYMFLKE0"
+};
+
+// Initialize Firebase
+export const firebaseapp = initializeApp(firebaseConfig);
